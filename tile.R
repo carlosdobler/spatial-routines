bff1df7db64e0020bf90fdac0ca53c628e6c274a
Carlos Dobler <dobler.carlos@gmail.com>
2024-03-21 12:41:04 -0400

    Update tile.R

--- tile.R
+++ tile.R
@@ -2,29 +2,29 @@
 # s = stars object
 
 fn_tile <- function(s, tile_size) {
+  
+  tb <- 
+    imap(c(1,2) %>% set_names(c("x", "y")), function(dim_id, dim_name){
       
-      tb <- 
-        imap(c(1,2) %>% set_names(c("x", "y")), function(dim_id, dim_name){
-          
-          d <- 
-            dim(s)[dim_id] %>%
-            seq_len()
-          
-          n <- 
-            round(dim(s)[dim_id]/tile_size)
-          
-          l <-
-            split(d, 
-                  ceiling(d/(length(d)/n))) %>% 
-            map_dfr(~tibble(start = first(.x), end = last(.x))) %>% 
-            rename(!!str_glue("start_{dim_name}") := 1,
-                   !!str_glue("end_{dim_name}") := 2)
-          
-          return(l)
-          
-        })
+      d <- 
+        dim(s)[dim_id] %>%
+        seq_len()
       
-      expand_grid(tb$x, tb$y) %>%
-        mutate(tile_id = row_number() %>% str_pad(width = 8, "left", "0"))
+      n <- 
+        round(dim(s)[dim_id]/tile_size)
       
-    }
+      l <-
+        split(d, 
+              ceiling(d/(length(d)/n))) %>% 
+        map_dfr(~tibble(start = first(.x), end = last(.x))) %>% 
+        rename(!!str_glue("start_{dim_name}") := 1,
+               !!str_glue("end_{dim_name}") := 2)
+      
+      return(l)
+      
+    })
+  
+  expand_grid(tb$x, tb$y) %>%
+    mutate(tile_id = row_number() %>% str_pad(width = 8, "left", "0"))
+  
+}
