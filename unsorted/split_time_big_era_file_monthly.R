59806631feb9c24c2e361d53e0f644ac1e39d9d1
Carlos Dobler <dobler.carlos@gmail.com>
2024-10-03 10:44:51 -0400

    Update split_time_big_era_file_monthly.R

--- unsorted/split_time_big_era_file_monthly.R
+++ unsorted/split_time_big_era_file_monthly.R
@@ -1,21 +1,29 @@
+url <- "https://object-store.os-api.cci2.ecmwf.int/cci2-prod-cache/5526e8b33210adf86bc47c3825e7b330.nc"
+date_i <- "1970-01-01"
+date_f <- "1970-12-01"
+var <- "total_precipitation"
+
+
 library(tidyverse)
 library(stars)
 
 source("https://raw.github.com/carlosdobler/spatial-routines/master/general_tools.R")
 
 # download big ERA5 file
-"https://object-store.os-api.cci2.ecmwf.int/cci2-prod-cache/ff0d06f2a6b90cad51a5a67e61354b11.nc" %>% 
-  download.file(destfile = "/mnt/pers_disk/era5_pot_evap.nc", quiet = T, method = "wget")
+
+
+url %>% 
+  download.file(destfile = "/mnt/pers_disk/tmp/temp.nc", quiet = T, method = "wget")
 
 dates <- 
-  seq(as_date("1986-01-01"), as_date("2024-08-01"), by = "1 month")
+  seq(as_date(date_i), as_date(date_f), by = "1 month")
 
 iwalk(dates, \(x, i){
   
   print(x)
   
   a <- 
-    read_ncdf("/mnt/pers_disk/era5_pot_evap.nc",
+    read_ncdf("/mnt/pers_disk/tmp/temp.nc",
               ncsub = cbind(start = c(1,1,i),
                             count = c(NA,NA,1))) %>% 
     suppressMessages()
@@ -24,12 +32,12 @@ iwalk(dates, \(x, i){
   print(ii)
   
   rt_write_nc(adrop(a),
-              str_glue("/mnt/pers_disk/era5_potential-evaporation_mon_{x}.nc"))
+              str_glue("/mnt/pers_disk/tmp/era5_{str_replace(var, '_', '-')}_mon_{x}.nc"))
   
-  "gsutil mv /mnt/pers_disk/era5_potential-evaporation_mon_{x}.nc gs://clim_data_reg_useast1/era5/monthly_means/potential_evaporation/" %>% 
+  "gsutil mv /mnt/pers_disk/tmp/era5_{str_replace(var, '_', '-')}_mon_{x}.nc gs://clim_data_reg_useast1/era5/monthly_means/{var}/" %>% 
     str_glue() %>% 
-    system(ignore.stdout = T, ignore.stderr = T)
+    system()
   
 })
 
-fs::file_delete("/mnt/pers_disk/era5_pot_evap.nc")
+fs::file_delete("/mnt/pers_disk/tmp/temp.nc")
