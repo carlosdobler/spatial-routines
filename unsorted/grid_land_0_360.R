18a7ba201b2450f635e04d384d1962d53162febe
Carlos Dobler <dobler.carlos@gmail.com>
2025-02-24 18:18:04 -0600

    Update grid_land_0_360.R

--- unsorted/grid_land_0_360.R
+++ unsorted/grid_land_0_360.R
@@ -31,3 +31,28 @@ land_r <-
 land_r[land_r == 0] <- NA
 
 st_dimensions(land_r) <- st_dimensions(s)[1:2]
+
+# ******
+
+# V2
+
+land_pol <- "/mnt/bucket_mine/misc_data/physical/ne_110m_land/ne_110m_land.shp" |> st_read()
+
+land_r <- 
+  land_pol %>% 
+  mutate(a = 1) %>% 
+  select(a) %>% 
+  st_rasterize(st_as_stars(st_bbox(),
+                           dx = 0.1,
+                           values = 0))
+
+land_r <- 
+  land_r %>% 
+  st_warp(s[,,,1] |> st_as_stars() |> adrop(),
+          method = "max",
+          use_gdal = T) %>% 
+  setNames("land")
+
+land_r[land_r == 0] <- NA
+
+st_dimensions(land_r) <- st_dimensions(s)[1:2]
