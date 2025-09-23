f5c47988734aaf1915190768783d1af28f95f12e
Carlos Dobler <dobler.carlos@gmail.com>
2025-09-23 15:47:32 -0600

    fixed engine conditional; fixed str_glue under future

--- general_tools.R
+++ general_tools.R
@@ -44,11 +44,15 @@ rt_gs_download_files <- function(f, dest, quiet = F, parallel = T, gsutil = F) {
     #
   } else {
     # identify the parallel engine to use
-    if("mirai" %in% installed.packages()){
+    if ("mirai" %in% installed.packages()) {
       if (parallel & mirai::daemons()$connections > 0) {
         parallel_ <- "m"
       } else {
-        next
+        if (parallel & !is(future::plan(), "sequential")) {
+          parallel_ <- "f"
+        } else {
+          parallel_ <- "none"
+        }
       }
     } else if (parallel & !is(future::plan(), "sequential")) {
       parallel_ <- "f"
@@ -98,7 +102,7 @@ rt_gs_download_files <- function(f, dest, quiet = F, parallel = T, gsutil = F) {
 
       f |>
         furrr::future_walk(\(f_) {
-          stringr::str_glue("{cmd} cp {f_} {dest}") |>
+          stringr::str_glue("{cmd} cp {f_} {dest}", cmd = cmd, dest = dest) |>
             system(ignore.stdout = T, ignore.stderr = T)
         })
     }
